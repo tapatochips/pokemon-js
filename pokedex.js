307bd4c7b7a4283c01f4474bd1049ba9c73bec54
Daniel Richmond <danielrichmond@Daniels-MacBook-Pro.local>
2023-04-27 16:59:18 -0500

    first commit with favicon as well

---
+++ pokedex.js
@@ -0,0 +1,31 @@
+const getPokemon = async () => {
+	event.preventDefault();
+	const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+	const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`;
+  
+	try {
+	  const response = await fetch(url);
+	  const data = await response.json();
+  
+	  const resultsDiv = document.getElementById("results");
+	  resultsDiv.innerHTML = `
+		<div class="card mb-3">
+		  <h5 class="card-header" id="name"> ${data.name}</h5>
+		  <div class="card-body">
+			<img class="card-img-top" src="${data.sprites.front_default}" alt="${data.name}" />
+			<p class="card-text"><strong>HP:</strong> ${data.stats.find(stat => stat.stat.name === "hp").base_stat}</p>
+			<p class="card-text"><strong>Attack:</strong> ${data.stats.find(stat => stat.stat.name === "attack").base_stat}</p>
+			<p class="card-text"><strong>Defense:</strong> ${data.stats.find(stat => stat.stat.name === "defense").base_stat}</p>
+			<p class="card-text"><strong>Type:</strong> ${data.types.map(type => type.type.name).join(", ")}</p>
+			<p class="card-text"><strong>Abilities:</strong></p>
+			<ul class="list-group list-group-flush">
+			  ${data.abilities.map(ability => `<li class="list-group-item">${ability.ability.name}</li>`).join("")}
+			</ul>
+		  </div>
+		</div>
+	  `;
+	} catch (error) {
+	  console.error(error);
+	}
+  };
+  
